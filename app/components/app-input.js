19cda33656e9530b272c7086a6587c82759cb5cb
Rick Guyer <ricoguyer@gmail.com>
2016-09-02 14:41:51 -0500

    Update component to check validation endpoint on field changes

--- app/components/app-input.js
+++ app/components/app-input.js
@@ -1,15 +1,38 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
 
-const { computed, get } = Ember;
+const { computed, get, observer, set } = Ember;
 
 export default Ember.Component.extend({
+  validation: Ember.inject.service(),
+
   fieldName: null,
   model: null,
   label: null,
   value: null,
+  serverErrors: null,
+  validationFields: null,
 
-  errors: computed('modelErrors', function() {
+  init() {
+    this._super(...arguments);
+    this.set('serverErrors', []);
+    this.set('validationFields', []);
+    this.get('triggerCheckInput');
+  },
+
+  actions: {
+    validate() {
+      this.get('checkInput').perform(this.get('value'));
+    }
+  },
+
+  errors: computed('serverErrors', 'modelErrors', function() {
+    let serverErrors = get(this, 'serverErrors');
     let modelErrors = get(this, 'modelErrors');
+
+    if (serverErrors.length) {
+      return serverErrors;
+    }
 
     return modelErrors;
   }),
@@ -18,4 +41,28 @@ export default Ember.Component.extend({
     return get(this, 'model').get('errors').errorsFor(get(this, 'fieldName'));
   }),
 
+  triggerCheckInput: observer('value', function() {
+    this.get('checkInput').perform(this.get('value'), true);
+  }),
+
+  checkInput: task(function *(value, delay) {
+    if (value === undefined) {
+      return null;
+    }
+
+    if (delay) {
+      yield timeout(500);
+    }
+    this.set('modelErrors', null);
+    let { state, response } = yield this.get('validation').check(this.get('model'), this.get('fieldName'), value);
+    if (state === 'error') {
+      set(this, 'serverErrors', response);
+      set(this, 'validationFields', []);
+      return response;
+    } else {
+      set(this, 'serverErrors', []);
+      set(this, 'validationFields', response);
+      return response;
+    }
+  }).restartable(),
 });
